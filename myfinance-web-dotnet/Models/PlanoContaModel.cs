d482b3b1d72f5700e2be72188ee3a50a19b2f3b3
Francesco Sacco <FSacco@slb.com>
2026-08-26 18:19:48 -0300

    Unidade 3.4 - Tela de Listagem de Plano de Contas

---
+++ myfinance-web-dotnet/Models/PlanoContaModel.cs
@@ -0,0 +1,10 @@
+
+namespace myfinance_web_dotnet.Models
+{
+    public class PlanoContaModel
+    {
+        public int? Id { get; set; }
+        public String Descricao { get; set; }
+        public String Tipo { get; set; }
+    }
+}
