09be4a3da2c6d4c42b27d29377300cb25fbf4925
Francesco Sacco <FSacco@slb.com>
2026-08-27 15:10:49 -0300

    Unidade 3.7 - Tela de Cadastro de Transações Financeiras

--- myfinance-web-dotnet/Models/TransacaoModel.cs
+++ myfinance-web-dotnet/Models/TransacaoModel.cs
@@ -1,4 +1,4 @@
-
+using Microsoft.AspNetCore.Mvc.Rendering;
 using myfinance_web_dotnet_domain.Entities;
 
 namespace myfinance_web_dotnet.Models
@@ -10,7 +10,7 @@ namespace myfinance_web_dotnet.Models
         public DateTime Data { get; set; }
         public decimal Valor { get; set; }
         public int PlanoContaId { get; set; }
-        public String Tipo { get; set; }
-        public PlanoConta PlanoConta { get; set; }
+        public String? Tipo { get; set; }
+        public IEnumerable<SelectListItem> ListaPlanoContas { get; set; }
     }
 }
